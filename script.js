092143f186088957f5084d272bdf49353a9e52ca
Patrick Zhou <patrickchang44543@gmail.com>
2024-06-10 10:37:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
 async function downloadImage(imageSrc) {
-    const image = await fetch(imageSrc);
-    const imageBlog = await image.blob();
-    const imageURL = URL.createObjectURL(imageBlog);
+  const image = await fetch(imageSrc);
+  const imageBlog = await image.blob();
+  const imageURL = URL.createObjectURL(imageBlog);
 
-    const link = document.createElement("a");
-    link.href = imageURL;
-    link.download = imageSrc.split("/")[6].split("?")[0];
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const link = document.createElement("a");
+  link.href = imageURL;
+  link.download = imageSrc.split("/")[6].split("?")[0];
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
 let next, ls, i, myPromise;
-next = document.querySelectorAll(".endButton-pLBGXH");
 goThrough = () => {
-    myPromise = new Promise(function (myResolve) {
-        i = 0;
-        next = document.querySelectorAll(".endButton-pLBGXH");
-        ls = document.querySelectorAll(".originalLink-Azwuo9");
-        var downloadInt = setInterval(() => {
-            downloadImage(ls[i].href);
-            i++;
-            if (i == ls.length) {
-                clearInterval(downloadInt);
-                myResolve();
-            }
-        }, 500);
+  myPromise = new Promise(function (myResolve) {
+    i = 0;
+    ls = [];
+      next=[];
+      document.querySelectorAll("div").forEach((el) => {
+  if (el.className.includes("endButton")) {
+    next.push(el);
+  }
+});
+    document.querySelectorAll("a").forEach((el) => {
+      if (el.className.includes("originalLink")) {
+        ls.push(el);
+      }
     });
-    myPromise.then(function () {
-        if (next[1].disabled) {
-            clearInterval(nextInt);
-        } else {
-            next[1].click();
-            setTimeout(() => {
-                goThrough();
-            }, 2000);
-        }
-    });
+    var downloadInt = setInterval(() => {
+      downloadImage(ls[i].href);
+      i++;
+      if (i == ls.length) {
+        clearInterval(downloadInt);
+        myResolve();
+      }
+    }, 500);
+  });
+  myPromise.then(function () {
+    if (next[1].disabled) {
+      clearInterval(nextInt);
+    } else {
+      next[1].click();
+      setTimeout(() => {
+        goThrough();
+      }, 9000);
+    }
+  });
 };
 goThrough();
